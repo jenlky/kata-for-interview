308382fcb8382a6df803fd304148b4b5edb2cbbe
Jenssen Lee <30688276+helplah@users.noreply.github.com>
2019-09-14 17:09:28 +0800

    Everything is working except for extractMax method

--- __tests__/data structures/maxHeap.test.js
+++ __tests__/data structures/maxHeap.test.js
@@ -1,4 +1,4 @@
-const { MaxHeap } = require("../maxHeap");
+const { MaxHeap } = require("../../data structures/maxHeap");
 
 let maxHeap;
 beforeEach(() => {
@@ -13,23 +13,60 @@ describe("MaxHeap", () => {
   describe("insert", () => {
     it("should insert an item", () => {
       maxHeap.insert(100);
-      expect(maxHeap.priorityQueue[0]).toEqual(100);
+      expect(maxHeap.priorityQueue[0]).toBe(100);
     });
 
     it("should insert multiple items", () => {
       maxHeap.insert(100);
       maxHeap.insert(10);
       maxHeap.insert(20);
-      expect(maxHeap.priorityQueue[0]).toEqual(100);
-      expect(maxHeap.priorityQueue[1]).toEqual(10);
-      expect(maxHeap.priorityQueue[2]).toEqual(20);
+      expect(maxHeap.priorityQueue[0]).toBe(100);
+      expect(maxHeap.priorityQueue[1]).toBe(10);
+      expect(maxHeap.priorityQueue[2]).toBe(20);
     });
 
-    xit("should swap child with parent if child is greater than parent", () => {
+    it("should recursively swap child with parent if child is greater than parent", () => {
       maxHeap.insert(10);
       maxHeap.insert(100);
-      expect(maxHeap.priorityQueue[0]).toEqual(100);
-      expect(maxHeap.priorityQueue[1]).toEqual(10);
+      maxHeap.insert(50);
+
+      maxHeap.insert(20);
+      maxHeap.insert(30);
+
+      maxHeap.insert(60);
+      maxHeap.insert(120);
+      expect(maxHeap.priorityQueue[0]).toBe(120);
+      expect(maxHeap.priorityQueue[1]).toBe(30);
+      expect(maxHeap.priorityQueue[2]).toBe(100);
+      expect(maxHeap.priorityQueue[3]).toBe(10);
+      expect(maxHeap.priorityQueue[4]).toBe(20);
+      expect(maxHeap.priorityQueue[5]).toBe(50);
+      expect(maxHeap.priorityQueue[6]).toBe(60);
     });
+  });
+
+  it("getMax should return the root node", () => {
+    maxHeap.insert(100);
+    maxHeap.insert(300);
+    expect(maxHeap.getMax()).toBe(300);
+  });
+
+  it.only("extractMax should remove and return the root node", () => {
+    maxHeap.insert(10);
+    maxHeap.insert(100);
+    maxHeap.insert(50);
+
+    maxHeap.insert(20);
+    maxHeap.insert(30);
+
+    maxHeap.insert(60);
+    maxHeap.insert(120);
+    expect(maxHeap.extractMax()).toBe(120);
+    expect(maxHeap.priorityQueue[0]).toBe(100);
+    expect(maxHeap.priorityQueue[1]).toBe(30);
+    expect(maxHeap.priorityQueue[2]).toBe(60);
+    expect(maxHeap.priorityQueue[3]).toBe(10);
+    expect(maxHeap.priorityQueue[4]).toBe(20);
+    expect(maxHeap.priorityQueue[5]).toBe(50);
   });
 });
--- data structures/maxHeap.js
+++ data structures/maxHeap.js
@@ -3,27 +3,70 @@ class MaxHeap {
     this.priorityQueue = [];
   }
 
-  getMax() {}
+  getMax() {
+    return this.priorityQueue[0];
+  }
 
-  extractMax() {}
+  extractMax() {
+    const rootNode = this.priorityQueue[0];
+    this.priorityQueue[0] = this.priorityQueue[this.priorityQueue.length - 1];
+    this.priorityQueue.pop();
+
+    let parentIndex = 0;
+    let parent = this.priorityQueue[parentIndex];
+    let firstChildIndex = 2 * parentIndex + 1;
+    let firstChild = this.priorityQueue[firstChildIndex];
+    let secondChildIndex = 2 * parentIndex + 2;
+    let secondChild = this.priorityQueue[secondChildIndex];
+
+    let length = this.priorityQueue.length;
+    while (parentIndex < length) {
+      let largerChild = firstChild > secondChild ? firstChild : secondChild;
+      let largerChildIndex =
+        largerChild === firstChild ? firstChildIndex : secondChildIndex;
+
+      if (parent < largerChild) {
+        this.priorityQueue[parentIndex] = largerChild;
+        this.priorityQueue[largerChildIndex] = parent;
+        parentIndex = largerChildIndex;
+      }
+
+      firstChildIndex = 2 * parentIndex + 1;
+      secondChildIndex = 2 * parentIndex + 2;
+      parent = this.priorityQueue[parentIndex];
+      firstChild = this.priorityQueue[firstChildIndex];
+      if (secondChildIndex < this.priorityQueue.length) {
+        secondChild = this.priorityQueue[secondChildIndex];
+      }
+
+      console.log("firstChild", firstChild);
+      console.log("secondChild", secondChild);
+      length--;
+    }
+    console.log(this.priorityQueue);
+    return rootNode;
+  }
 
   insert(item) {
-    if (this.priorityQueue.length > 0) {
-      const parentIndex = this.priorityQueue.length - 1 / 2;
-      console.log(parentIndex);
+    this.priorityQueue.push(item);
+    let itemIndex = this.priorityQueue.length - 1;
 
-      const parent = this.priorityQueue[parentIndex];
-
-      console.log("item", item);
-      console.log("parent", parent);
-
-      if (item > parent) {
-        this.priorityQueue[parentIndex] = item;
-        this.priorityQueue.push(parent);
-      }
+    let itemParentIndex;
+    if (itemIndex > 0) {
+      itemParentIndex = Math.floor((itemIndex - 1) / 2);
+    } else {
+      itemParentIndex = Math.ceil((itemIndex - 1) / 2);
     }
 
-    this.priorityQueue.push(item);
+    let parent = this.priorityQueue[itemParentIndex];
+    while (item > parent) {
+      this.priorityQueue[itemParentIndex] = item;
+      this.priorityQueue[itemIndex] = parent;
+
+      itemIndex = itemParentIndex;
+      itemParentIndex = Math.floor((itemIndex - 1) / 2);
+      parent = this.priorityQueue[itemParentIndex];
+    }
   }
 
   delete() {}
